0c89692e46af503def53851f57bbf1e005e7820f
SakarayAnilKumar <anilirctc26@gmail.com>
2026-07-09 22:13:50 +0530

    Create app.py to generate results.txt file

---
+++ app.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+print("Python artifact demo")
+
+with open("results.txt","W") as file:
+  file.write("Hello from github\n")
+  file.write("This file is created in git\n")
+  file.write(f"Generated time is {datetime.now()}\n")
+  
+print("results.txt file is created successfully)
